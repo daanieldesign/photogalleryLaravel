287d1e5001c73050fc4e7c9b8531a25170f078f3
Daniel <mahdakdaniel@gmail.com>
2025-01-31 00:56:57 +0100

    again

--- galerie-app/public/index.php
+++ galerie-app/public/index.php
@@ -4,11 +4,18 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-require __DIR__ . '../index.php';
+// Composer autoload
+require __DIR__ . '/../vendor/autoload.php';
 
-// Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
+// Bootstrap Laravel app
+$app = require_once __DIR__ . '/../bootstrap/app.php';
 
-// Bootstrap Laravel and handle the request...
-(require_once __DIR__.'/../bootstrap/app.php')
-    ->handleRequest(Request::capture());
+// Handle the request
+$request = Request::capture();
+$response = $app->handle($request);
+
+// Send the response
+$response->send();
+
+// Terminate the application
+$app->terminate($request, $response);
