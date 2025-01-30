d7126d60558b7ffe2c4c4b72c22c32456a3ceb90
Daniel <mahdakdaniel@gmail.com>
2025-01-31 00:48:15 +0100

    again

--- galerie-app/public/index.php
+++ galerie-app/public/index.php
@@ -4,11 +4,6 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
-}
-
 require __DIR__ . '../index.php';
 
 // Register the Composer autoloader...
